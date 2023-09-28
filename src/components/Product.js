29a62b4737718ee234bdfdec5f6fb03918032b9f
leobewater <mmbee888@gmail.com>
2023-09-28 17:14:11 -0400

    populate product listings

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Product = () => {
   const [products, setProducts] = useState([]);
@@ -9,10 +11,31 @@ const Product = () => {
       .then((result) => setProducts(result));
   }, []);
 
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{ marginBottom: '10px' }}>
+      <Card key={product.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: '100px', height: '130px' }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>${product.price}</Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ background: 'white' }}>
+          <Button variant="primary">Add To Cart</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
+
   return (
     <>
       <h1>Product Dashboard</h1>
-      {JSON.stringify(products)}
+      <div className="row">{cards}</div>
     </>
   );
 };
