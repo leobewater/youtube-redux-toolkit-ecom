77db85ef9626e1b86a72a61275d7bf140c685355
leobewater <mmbee888@gmail.com>
2023-09-28 17:05:52 -0400

    fetch products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,20 @@
-const Product = () => {
-  return (
-    <h1>Product Dashboard</h1>
-  )
-}
+import { useEffect, useState } from 'react';
 
-export default Product
+const Product = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((data) => data.json())
+      .then((result) => setProducts(result));
+  }, []);
+
+  return (
+    <>
+      <h1>Product Dashboard</h1>
+      {JSON.stringify(products)}
+    </>
+  );
+};
+
+export default Product;
