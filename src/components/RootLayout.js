f6d62ca88fd6462c74cf7b61130ea68e29d33a72
leobewater <mmbee888@gmail.com>
2023-09-28 17:48:22 -0400

    fix store import

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,19 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import NavbarPanel from './NavbarPanel';
 import { Provider } from 'react-redux';
-import { store } from '../store/store';
+import store from '../store/store';
 
 const RootLayout = () => {
   return (
-    <>
-      <Provider store={store}>
-        <NavbarPanel />
+    <Provider store={store}>
+      <NavbarPanel />
 
-        <main>
-          <Outlet />
-        </main>
-      </Provider>
-    </>
+      <main>
+        <Outlet />
+      </main>
+    </Provider>
   );
 };
 
