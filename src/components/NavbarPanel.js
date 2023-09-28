8d93218e08f9eb399ea6befd7a0a4ba72fed7153
leobewater <mmbee888@gmail.com>
2023-09-28 17:30:37 -0400

    set up cart link

--- src/components/NavbarPanel.js
+++ src/components/NavbarPanel.js
@@ -8,17 +8,18 @@ const NavbarPanel = () => {
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
         <Navbar.Brand href="#">Redux Toolkit</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link to="/" as={Link}>
-              Products
+        <Nav>
+          <Nav.Link to="/" as={Link}>
+            Products
+          </Nav.Link>
+        </Nav>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <Nav.Link to="/cart" as={Link}>
+              My Bag 0
             </Nav.Link>
-          </Nav>
+          </Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
