1ed6554741e848606aa9a36cad7de4e4ec3d7c9f
leobewater <mmbee888@gmail.com>
2023-09-29 07:06:48 -0400

    remove item logic

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,20 +1,23 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { useDispatch } from 'react-redux';
-import { add } from '../store/cartSlice';
+import { remove } from '../store/cartSlice';
 
 const Cart = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.cart);
 
-  const removeItem = (product) => {
-    // dispatch an add action
-    dispatch(add(product));
+  const removeItem = (id) => {
+    // dispatch a remove action
+    dispatch(remove(id));
   };
 
   const cards = products.map((product) => (
-    <div key={product.id} className="col-md-12" style={{ marginBottom: '10px' }}>
+    <div
+      key={product.id}
+      className="col-md-12"
+      style={{ marginBottom: '10px' }}
+    >
       <Card className="h-100">
         <div className="text-center">
           <Card.Img
@@ -28,7 +31,7 @@ const Cart = () => {
           <Card.Text>${product.price}</Card.Text>
         </Card.Body>
         <Card.Footer style={{ background: 'white' }}>
-          <Button variant="danger" onClick={() => removeItem(product)}>
+          <Button variant="danger" onClick={() => removeItem(product.id)}>
             Remove Item
           </Button>
         </Card.Footer>
