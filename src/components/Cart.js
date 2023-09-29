12032b7a1c6eb69829d833b70a247be7fda44f33
leobewater <mmbee888@gmail.com>
2023-09-29 07:04:06 -0400

    populate the cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useDispatch } from 'react-redux';
+import { add } from '../store/cartSlice';
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.cart);
 
-export default Cart
+  const removeItem = (product) => {
+    // dispatch an add action
+    dispatch(add(product));
+  };
+
+  const cards = products.map((product) => (
+    <div key={product.id} className="col-md-12" style={{ marginBottom: '10px' }}>
+      <Card className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: '100px', height: '130px' }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>${product.price}</Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ background: 'white' }}>
+          <Button variant="danger" onClick={() => removeItem(product)}>
+            Remove Item
+          </Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
+
+  return (
+    <>
+      <h1>Cart</h1>
+      <div className="row">{cards}</div>
+    </>
+  );
+};
+
+export default Cart;
